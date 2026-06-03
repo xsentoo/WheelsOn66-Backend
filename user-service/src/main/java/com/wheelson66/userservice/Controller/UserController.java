65be514d41ff73ae26000a9fece35a0915af53e7
senthooran <123653047+xsentoo@users.noreply.github.com>
2026-06-03 14:31:37 +0200

    Correction de login et regiter

--- user-service/src/main/java/com/wheelson66/userservice/Controller/UserController.java
+++ user-service/src/main/java/com/wheelson66/userservice/Controller/UserController.java
@@ -1,40 +1,30 @@
 package com.wheelson66.userservice.Controller;
 
+import com.wheelson66.userservice.Dto.LoginRequest;
 import com.wheelson66.userservice.Dto.RegisterRequest;
+import com.wheelson66.userservice.Dto.UserResponse;
 import com.wheelson66.userservice.Entity.User;
-import com.wheelson66.userservice.Repository.UserRepository;
-import com.wheelson66.userservice.Service.JwtService;
 import com.wheelson66.userservice.Service.UserService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
+    private final UserService userService;
 
-    private final  UserService userService;
-    private  final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final JwtService jwtService;
-
-    public UserController(UserService userService, UserRepository userRepository, PasswordEncoder passwordEncoder, JwtService jwtService) {
+    public UserController(UserService userService) {
         this.userService = userService;
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-        this.jwtService = jwtService;
     }
 
     @PostMapping("/register")
-    public User CreateUser(@RequestBody RegisterRequest request){
-        return userService.registerUser(request.getEmail(), request.getRawPassword(), request.getPseudo());
+    public UserResponse CreateUser(@RequestBody RegisterRequest request){
+        User savedUser = userService.registerUser(request.getEmail(), request.getRawPassword(), request.getPseudo());
+        return new UserResponse(savedUser.getId(), savedUser.getEmail(), savedUser.getPseudo());
     }
 
     @PostMapping("/login")
-    public String login(@RequestBody RegisterRequest request){
+    public String login(@RequestBody LoginRequest request){
         return userService.loginUser(request.getEmail(), request.getRawPassword());
     }
-
-
 }
--- user-service/src/main/java/com/wheelson66/userservice/Service/JwtService.java
+++ user-service/src/main/java/com/wheelson66/userservice/Service/JwtService.java
@@ -3,21 +3,32 @@ package com.wheelson66.userservice.Service;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
 
 @Service
 public class JwtService {
-    private static final Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    @Value("${jwt.secret}")
+    private String secretKeyString;
+
+    @Value("${jwt.expiration}")
+    private Long jwtExpiration;
+
+    private Key getSigningKey(){
+        byte[]keyBytes = this.secretKeyString.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 
     public String generateToken(String email) {
         return Jwts.builder()
                 .setSubject(email)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24)) // Valide 24h
-                .signWith(SECRET_KEY)
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .signWith(getSigningKey(),SignatureAlgorithm.HS256)
                 .compact();
     }
 }
